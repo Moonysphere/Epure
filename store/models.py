8ef1b3bfbe704898acb5bed606010a47b600c43d
Baptiste ROY <25baptisteroy@gmail.com>
2025-01-21 15:46:14 +0100

    add des dernirer fonction

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+from epure.settings import AUTH_USER_MODEL
 """
 Product
 -name
@@ -13,7 +15,7 @@ Product
 
 
 # Create your models here.
-
+#user
 class Product(models.Model) :
     name = models.CharField(max_length=128)
     slug = models.SlugField(max_length=128)
@@ -27,3 +29,50 @@ class Product(models.Model) :
     
     def get_absolute_url(self):
         return reverse("product", kwargs={"slug": self.slug})
+    
+    #order
+    """
+    -utilisateur
+    -Produit
+    -count
+    -commander ou non 
+    """
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity})"
+
+
+
+
+
+    #cart
+
+    """
+    -utlisateur
+    -articles
+    -commande ou non
+    -date commande 
+    """
+
+class Cart(models.Model):
+        user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+        orders = models.ManyToManyField(Order)
+
+        def __str__(self):
+            return self.user.username
+        
+
+def delete(self, *args, **kwargs):
+     for order in self.orders.all():
+          order.ordered = True
+          order.ordered_sate = timezone.now()
+
+
+
+     super().delete(*args, **kwargs)
